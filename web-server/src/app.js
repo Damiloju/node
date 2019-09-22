097215e404b8d3e49f525c07da05c66aaddde48a
Yusuf Damiloju <--add>
2019-09-22 23:23:44 +0100

    finish final work on web servers and weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -76,6 +77,6 @@ app.get("*", (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log("Server running on port 3000");
+app.listen(port, () => {
+	console.log("Server running on port " + port);
 });
