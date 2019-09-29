e4d369a509f7fb92f025db8d1047e7a0a6141d60
Yusuf Damiloju <--add>
2019-09-29 11:56:13 +0100

    Work in Progress; start and finish  work on the mongodb module

---
+++ task-manager/mongodb.js
@@ -0,0 +1,26 @@
+const { MongoClient, ObjectId } = require("mongodb");
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(connectionURL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+	.then(client => {
+		const db = client.db(databaseName);
+
+		db.collection("tasks")
+			.findOne({ _id: new ObjectId("5d88e155bbf87e42d0fb617a") })
+			.then(result => {
+				console.log(result);
+			})
+			.catch(err => {
+				console.log("Could not add record to database", err);
+			});
+
+		client.close();
+	})
+	.catch(err => {
+		console.log("Unable to connect to database", err);
+	});
