ac3038f0b0637ed56c98a001c119419dc99aaceb
Oluwadamiloju <yusufdamiloju@gmail.com>
2019-11-26 19:27:23 +0100

    learning validation

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,29 +1,67 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
+const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
 
 mongoose.connect(connectionURL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
+const User = mongoose.model("User", {
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is not valid");
+      }
     }
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
+  }
 });
 
 const bolanle = new User({
-    name: "Bolanle",
-    age: 45
+  name: "Bolanle",
+  email: "example@mail.com",
+  age: 45
 });
 
-bolanle.save().then(res => {
+bolanle
+  .save()
+  .then(res => {
     console.log(res);
-}).catch(err => {
+  })
+  .catch(err => {
     console.log("Error!", err);
+  });
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  }
 });
+
+const finishPlus = new Task({
+  description: "We have to finish t-plus my dear",
+  completed: false
+});
+
+finishPlus
+  .save()
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
