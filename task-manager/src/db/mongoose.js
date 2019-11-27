a538b448237329199bbe7b7fded08e5077c5b22a
Oluwadamiloju <yusufdamiloju@gmail.com>
2019-11-27 05:22:41 +0100

    add emdpoint for creating tasks

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -7,15 +7,3 @@ mongoose.connect(connectionURL, {
     useCreateIndex: true,
     useUnifiedTopology: true
 });
-
-const Task = mongoose.model("Task", {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.port || 3000;
@@ -11,6 +12,16 @@ app.post("/users", (req, res) => {
     const user = new User(req.body);
 
     user.save().then((response) => {
+        res.send(response);
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
+});
+
+app.post("/tasks", (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then((response) => {
         res.send(response);
     }).catch((err) => {
         res.status(400).send(err);
