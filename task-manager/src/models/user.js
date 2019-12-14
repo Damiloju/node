08afe3d21733e6355a5734c96eabbc0ba02cd8eb
Oluwadamiloju <yusufdamiloju@gmail.com>
2019-12-15 00:39:20 +0100

    cascade delete

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -108,6 +109,16 @@ userSchema.pre("save", async function (next) {
     if (user.isModified("password")) {
         user.password = await bcrypt.hash(user.password, 8);
     }
+
+    next();
+});
+
+userSchema.pre("remove", async function (next) {
+    const user = this;
+
+    await Task.deleteMany({
+        owner: user._id
+    });
 
     next();
 });
