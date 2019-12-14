268040192da8bd1d81666969a07990977df0897d
Oluwadamiloju <yusufdamiloju@gmail.com>
2019-12-14 23:36:25 +0100

    private data now hidden

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -66,6 +66,17 @@ userSchema.statics.findByCredentials = async (email, password) => {
     return user;
 }
 
+userSchema.methods.toJSON = function () {
+    const user = this;
+
+    const userObject = user.toObject();
+
+    delete userObject.password;
+    delete userObject.tokens;
+
+    return userObject;
+}
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
 
