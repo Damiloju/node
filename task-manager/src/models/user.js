4030e782e96b280602ffd8a02d67e513616ff941
Oluwadamiloju <yusufdamiloju@gmail.com>
2019-12-15 14:48:31 +0100

    timestamps now added

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,126 +1,137 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
-const Task = require('./task');
+const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowerCase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Email is not valid");
-            }
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowerCase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid");
         }
+      }
     },
     password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 6,
-        validate(value) {
-            if (validator.contains(value.toLowerCase(), "password")) {
-                throw new Error("Password cannot not contain password");
-            }
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+      validate(value) {
+        if (validator.contains(value.toLowerCase(), "password")) {
+          throw new Error("Password cannot not contain password");
         }
+      }
     },
     age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error("Age must be a positive number");
-            }
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be a positive number");
         }
+      }
     },
-    tokens: [{
+    tokens: [
+      {
         token: {
-            type: String,
-            required: true
+          type: String,
+          required: true
         }
-    }]
-});
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
 
 userSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({
-        email
-    });
+  const user = await User.findOne({
+    email
+  });
 
-    if (!user) {
-        throw new Error('Invalid Credentials');
-    }
+  if (!user) {
+    throw new Error("Invalid Credentials");
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        throw new Error("Invalid Credentials");
-    }
+  if (!isMatch) {
+    throw new Error("Invalid Credentials");
+  }
 
-    return user;
-}
+  return user;
+};
 
-userSchema.virtual('tasks', {
-    ref: 'Task',
-    localField: '_id',
-    foreignField: 'owner'
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner"
 });
 
-userSchema.methods.toJSON = function () {
-    const user = this;
+userSchema.methods.toJSON = function() {
+  const user = this;
 
-    const userObject = user.toObject();
+  const userObject = user.toObject();
 
-    delete userObject.password;
-    delete userObject.tokens;
+  delete userObject.password;
+  delete userObject.tokens;
 
-    return userObject;
-}
+  return userObject;
+};
 
-userSchema.methods.generateAuthToken = async function () {
-    const user = this;
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
 
-    const token = jwt.sign({
-        _id: user._id.toString()
-    }, 'thisisjwttokensecretpayloadfuckislongman', {
-        expiresIn: '2 days'
-    });
+  const token = jwt.sign(
+    {
+      _id: user._id.toString()
+    },
+    "thisisjwttokensecretpayloadfuckislongman",
+    {
+      expiresIn: "2 days"
+    }
+  );
 
-    user.tokens = user.tokens.concat({
-        token
-    });
+  user.tokens = user.tokens.concat({
+    token
+  });
 
-    await user.save();
+  await user.save();
 
-    return token;
-}
+  return token;
+};
 
 //Hash the plain text password before saving
-userSchema.pre("save", async function (next) {
-    const user = this;
+userSchema.pre("save", async function(next) {
+  const user = this;
 
-    if (user.isModified("password")) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
 
-    next();
+  next();
 });
 
-userSchema.pre("remove", async function (next) {
-    const user = this;
+userSchema.pre("remove", async function(next) {
+  const user = this;
 
-    await Task.deleteMany({
-        owner: user._id
-    });
+  await Task.deleteMany({
+    owner: user._id
+  });
 
-    next();
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
